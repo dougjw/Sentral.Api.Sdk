51b5b45683a33718d74f1810661ac9f1000ee484
Douglas Williams <dwilliams@loretonh.nsw.edu.au>
2021-05-25 16:43:04 +1000

    Added Consent Cmdlets

---
+++ Sentral.API.PowerShell/Enrolments/NewSntEnrConsent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using Sentral.API.Client;
+using Sentral.API.Model.Enrolments.Include;
+using Sentral.API.PowerShell;
+using Sentral.API.Model.Enrolments;
+using Sentral.API.PowerShell.Common;
+using Sentral.API.Model.Enrolments.Update;
+
+namespace Sentral.API.PowerShell.Enrolments
+{
+    [Cmdlet(VerbsCommon.New,"SntEnrConsent")]
+    [OutputType(typeof(Consent))]
+    public class NewSntEnrConsent : SentralPSCmdlet
+    {
+                  
+        [Parameter(Mandatory = true)]
+        public string ConsentGiven { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Details { get; set; }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+        }
+        
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            UpdateConsent consent = new UpdateConsent()
+            {
+
+                Details = Details,
+                ConsentGiven = ConsentGiven
+            };
+
+            // Populate from student object if object was used.
+ 
+
+            var response = SentralApiClient.Enrolments.CreateConsent(consent);
+
+            WriteObject(response);
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+        }
+    }
+}
---
+++ Sentral.API.PowerShell/Enrolments/SetSntEnrConsent.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using Sentral.API.Client;
+using Sentral.API.Model.Enrolments.Include;
+using Sentral.API.PowerShell;
+using Sentral.API.Model.Enrolments;
+using Sentral.API.PowerShell.Common;
+using Sentral.API.Model.Enrolments.Update;
+
+namespace Sentral.API.PowerShell.Enrolments
+{
+    [Cmdlet(VerbsCommon.Set,"SntEnrConsent")]
+    [OutputType(typeof(Consent))]
+    public class SetSntEnrConsent : SentralPSCmdlet
+    {
+
+        private string _consentGiven;
+        private string _details;
+
+        private bool _consentGivenProvided;
+        private bool _detailsProvided;
+
+
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ParameterSetName ="ConsentId")]
+        [ValidateRange(1, int.MaxValue)]
+        public int ConsentId { get; set; }
+
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ParameterSetName = "Consent")]
+        [ValidateRange(1, int.MaxValue)]
+        public Consent Consent { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string ConsentGiven
+        {
+            get
+            {
+                return _consentGiven;
+            }
+            set
+            {
+                _consentGiven = value;
+                _consentGivenProvided = true;
+            }
+
+        }
+
+        [Parameter(Mandatory = false)]
+        public string Details
+        {
+            get
+            {
+                return _details;
+            }
+            set
+            {
+                _details = value;
+                _detailsProvided = true;
+            }
+
+        }
+        
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+        }
+
+        private UpdateConsent GetInitUpdateStudent()
+        {
+
+            if (Consent != null)
+            {
+                return new UpdateConsent(Consent.ID);
+            }
+            return new UpdateConsent(ConsentId);
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            UpdateConsent consent = GetInitUpdateStudent(); 
+
+            // Populate from student object if object was used.
+            if (_detailsProvided)
+            {
+                consent.Details = _details;
+            };
+            if (_consentGivenProvided)
+            {
+                consent.ConsentGiven = _consentGiven;
+            }
+
+            var response = SentralApiClient.Enrolments.UpdateConsent(consent);
+
+            WriteObject(response);
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+        }
+    }
+}
